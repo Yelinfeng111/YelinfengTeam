ed5129d927258f3d4baf7784cced6cf6b9605094
yelinf73 <yelinf11@foxmail.com>
2018-02-25 15:45:13 +0800

    20180225 创建框架

---
+++ src/main/java/com/steam/mysteam/MysteamApplication.java
@@ -0,0 +1,12 @@
+package com.steam.mysteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysteamApplication.class, args);
+	}
+}
